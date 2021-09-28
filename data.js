10ac4e0c97cde5965d2b0f44aeeee4604557e43d
Dylan Floyd <floyd800@gmail.com>
2021-09-28 13:38:59 -0700

    add routes

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
 const cars = [
     {
+        id: 1,
         make: "Tesla",
         model: "Model 3",
         releaseYear: 2017,
@@ -7,6 +8,7 @@ const cars = [
         energyType: "electric"
     },
     {
+        id: 2,
         make: "Ford",
         model: "Mustang",
         releaseYear: 1964,
@@ -14,6 +16,7 @@ const cars = [
         energyType: "gas"
     },
     {
+        id: 3,
         make: "Mazda",
         model: "Mazda3",
         releaseYear: 2003,
@@ -21,6 +24,7 @@ const cars = [
         energyType: "gas"
     },
     {
+        id: 4,
         make: "Mazda",
         model: "Miata",
         releaseYear: 1989,
@@ -28,6 +32,7 @@ const cars = [
         energyType: "gas"
     },
     {
+        id: 5,
         make: "Toyota",
         model: "Prius",
         releaseYear: 1997,
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const { cars } = require('./data.js');
+const cors = require('cors');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(cors());
+
+app.get('/cars/:id', (req, res) => {
+    const car = cars.find(car => car.id === Number(req.params.id));
+    res.json(car);
+})
+
+app.get('/cars', (req, res) => {
+    res.json(cars);
 })
 
 module.exports = { app };
